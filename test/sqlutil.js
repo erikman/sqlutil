85bdff9c7129070b95be057a625415beb9d03710
Erik Man <goodbyte@gmail.com>
2017-02-04 21:49:24 +0100

    Initial commit

---
+++ test/sqlutil.js
@@ -0,0 +1,409 @@
+import chai from 'chai';
+import chaiAsPromised from 'chai-as-promised';
+import streamutil from 'streamutil';
+
+import * as sqlutil from '../lib/sqlutil';
+
+chai.use(chaiAsPromised);
+
+let assert = chai.assert;
+let expect = chai.expect;
+
+describe('sqlutil', () => {
+  let db;
+
+  describe('table', () => {
+    let table;
+
+    it('should be able to create a database in memory', () => {
+      db = new sqlutil.Db();
+      return db.open(':memory:');
+    });
+
+    it('should be possible to create new tables', () => {
+      table = new sqlutil.Table(db, {
+        name: 'testtable',
+        columns: {
+          id: {type: sqlutil.DataType.INTEGER, primaryKey: true},
+          name: {type: sqlutil.DataType.TEXT, unique: true},
+          value: {type: sqlutil.DataType.FLOAT}
+        }
+      });
+
+      return table.createTable();
+    });
+
+    it('should reject creating tables with unknown data types', () => {
+      let badTable = new sqlutil.Table(db, {
+        name: 'badtable',
+        columns: {
+          id: {type: sqlutil.DataType.INTEGER, primaryKey: true},
+          name: {type: sqlutil.DataType.BAD_TYPE}
+        }
+      });
+
+      return assert.isRejected(badTable.createTable());
+    });
+
+    it('should be possible to add rows to the table', () => {
+      return table.insert({name: 'key1', value: 42})
+        .then(() => {
+          return table.insert({name: 'key2', value: 42});
+        }).then(() => {
+          return table.insert({name: 'key3', value: 43});
+        });
+    });
+
+    it('should be possible to have default values for columns', () => {
+      let defaultValueTable = new sqlutil.Table(db, {
+        name: 'defaultValueTable',
+        columns: {
+          id: {type: sqlutil.DataType.INTEGER, primaryKey: true},
+          name: {type: sqlutil.DataType.TEXT, unique: true},
+          value: {type: sqlutil.DataType.FLOAT, notNull: true, defaultValue: 3.0}
+        }
+      });
+
+      return defaultValueTable.createTable()
+        .then(() => defaultValueTable.insert({name: 'keyWithValue', value: 1.0}))
+        .then(() => defaultValueTable.insert({name: 'keyWithoutValue'}))
+        .then(() => defaultValueTable.find({name: 'keyWithValue'}).get()
+              .then(keyWithValue => {
+                assert.equal(keyWithValue.value, 1.0);
+              }))
+        .then(() => defaultValueTable.find({name: 'keyWithoutValue'}).get()
+              .then(keyWithoutValue => {
+                assert.equal(keyWithoutValue.value, 3.0);
+              }));
+    });
+
+    // Zero needs extra care since it is also 'false'
+    it('should be possible to have default value 0 for columns', () => {
+      let defaultValueTable = new sqlutil.Table(db, {
+        name: 'defaultValueTableZero',
+        columns: {
+          id: {type: sqlutil.DataType.INTEGER, primaryKey: true},
+          name: {type: sqlutil.DataType.TEXT, unique: true},
+          value: {type: sqlutil.DataType.INTEGER, notNull: true, defaultValue: 0}
+        }
+      });
+
+      return defaultValueTable.createTable()
+        .then(() => defaultValueTable.insert({name: 'keyWithValue', value: 1}))
+        .then(() => defaultValueTable.insert({name: 'keyWithoutValue'}))
+        .then(() => defaultValueTable.find({name: 'keyWithValue'}).get()
+              .then(keyWithValue => {
+                assert.equal(keyWithValue.value, 1);
+              }))
+        .then(() => defaultValueTable.find({name: 'keyWithoutValue'}).get()
+              .then(keyWithoutValue => {
+                assert.equal(keyWithoutValue.value, 0);
+              }));
+    });
+
+    it('should be possible to have non-unique indices', () => {
+      let indexTable = new sqlutil.Table(db, {
+        name: 'indexTable',
+        columns: {
+          id: {type: sqlutil.DataType.INTEGER, primaryKey: true},
+          name: {type: sqlutil.DataType.TEXT, unique: true},
+          value: {type: sqlutil.DataType.FLOAT, notNull: true, index: true}
+        }
+      });
+
+      return indexTable.createTable();
+    });
+
+    it('should be possible to have index on multiple columns', () => {
+      let indexTable = new sqlutil.Table(db, {
+        name: 'multiIndexTable',
+        columns: {
+          id: {type: sqlutil.DataType.INTEGER, primaryKey: true},
+          name: {type: sqlutil.DataType.TEXT, unique: true},
+          value: {type: sqlutil.DataType.FLOAT, notNull: true, index: true}
+        },
+        indices: {
+          nameValue: {columns: ['name', 'value']}
+        }
+      });
+
+      return indexTable.createTable();
+    });
+
+
+    it('should be a failure to name non-existing columns as part of an index', () => {
+      let indexTable = new sqlutil.Table(db, {
+        name: 'multiIndexTableFail',
+        columns: {
+          id: {type: sqlutil.DataType.INTEGER, primaryKey: true},
+          name: {type: sqlutil.DataType.TEXT, unique: true},
+          value: {type: sqlutil.DataType.FLOAT, notNull: true, index: true}
+        },
+        indices: {
+          nameValue: {columns: ['nonExisting', 'value']}
+        }
+      });
+
+      return expect(indexTable.createTable()).to.eventually.be.rejected;
+    });
+
+    it('should be possible to retrieve single rows from the table', () => {
+      return table.find({name: 'key1'}).get().then(row => {
+        assert.isObject(row);
+        assert.equal(row.name, 'key1');
+        assert.equal(row.value, 42);
+      });
+    });
+
+    it('should be possible to retrieve multiple rows from the table', () => {
+      return table.find({value: 42}).all().then(rows => {
+        assert.isArray(rows);
+        assert.equal(rows.length, 2);
+        assert.equal(rows[0].name, 'key1');
+        assert.equal(rows[1].name, 'key2');
+      });
+    });
+
+    it('should be possible to iterate over rows from the table', () => {
+      let rowCount = 0;
+      return table.find({value: 42}).each(row => {
+        rowCount++;
+        assert(row.name === 'key1' || row.name === 'key2');
+      }).then(() => {
+        assert.equal(rowCount, 2);
+      });
+    });
+
+    it('should be possible to stream rows from the table', () => {
+      let sqlStream = table.find({value: 42}).stream();
+
+      assert.eventually.lengthOf(streamutil.streamToArray(sqlStream), 2);
+    });
+
+    it('should be possible to stream rows into a table', () => {
+      let sourceStream = streamutil.arrayToStream([
+        {name: 'key5', value: 62},
+        {name: 'key6', value: 63},
+        {name: 'key7', value: 64}
+      ]);
+
+      let sqlWriter = table.createWriteStream();
+
+      let sqlPipeline = streamutil.pipeline([
+        sourceStream,
+        sqlWriter
+      ]);
+
+      return streamutil.waitForStream(sqlPipeline);
+    });
+
+    it('should be possible to lookup unique fields with a stream', () => {
+      let sourceStream = streamutil.arrayToStream([
+        {name: 'key5'},
+        {name: 'key6'},
+        {name: 'key7', value: 1}, // Will override value from the table
+        {name: 'key8'} // does not exist
+      ]);
+
+      let sqlTransform = table.createExtendStream();
+
+      return streamutil.streamToArray(streamutil.pipeline([
+        sourceStream,
+        sqlTransform
+      ])).then(rows => {
+        assert.lengthOf(rows, 4);
+
+        assert.isNumber(rows[0].id);
+        assert.equal(rows[0].name, 'key5');
+        assert.equal(rows[0].value, 62);
+
+        assert.isNumber(rows[1].id);
+        assert.equal(rows[1].name, 'key6');
+        assert.equal(rows[1].value, 63);
+
+        assert.isNumber(rows[2].id);
+        assert.equal(rows[2].name, 'key7');
+        assert.equal(rows[2].value, 1); // overridden value
+
+        assert.isUndefined(rows[3].id);
+        assert.equal(rows[3].name, 'key8');
+        assert.isUndefined(rows[3].value);
+      });
+    });
+
+    it('should be possible to select using multiple matchers', () => {
+      return table.find({
+        name: 'key1',
+        value: 42
+      }).get().then(row => {
+        assert.isObject(row);
+        assert.equal(row.name, 'key1');
+        assert.equal(row.value, 42);
+      });
+    });
+
+    it('should be possible select using binary operators', () => {
+      let rowCount = 0;
+      return table.find({value: {$le: 42}}).each(row => {
+        rowCount++;
+        assert.isObject(row);
+        assert(row.name === 'key1' || row.name === 'key2');
+      }).then(() => {
+        assert.equal(rowCount, 2);
+      });
+    });
+
+
+    it('should be possible to select using explicit $and', () => {
+      return table.find({$and: [
+        {name: 'key1'},
+        {value: 42}
+      ]}).get().then(row => {
+        assert.equal(row.name, 'key1');
+        assert.equal(row.value, 42);
+      });
+    });
+
+    it('should not be possible to retrieve non-existing rows', () => {
+      return table.find({name: 'non-existing-key'}).get().then(row => {
+        assert.isUndefined(row);
+      });
+    });
+
+    it('should be possible to update rows', () => {
+      return table.insertUpdateUnique({name: 'key1', value: 50}).then(row => {
+        assert.isObject(row);
+        assert.equal(row.name, 'key1');
+        assert.equal(row.value, 50);
+      });
+    });
+
+    it('should be possible to update rows with null values', () => {
+      return table.insertUpdateUnique({name: 'key1', value: null}).then(row => {
+        assert.isObject(row);
+        assert.equal(row.name, 'key1');
+        assert.isNull(row.value);
+      });
+    });
+
+    it('should be possible to create unique rows', () => {
+      return table.insertUpdateUnique({name: 'new-unique-key', value: 33}).then(row => {
+        assert.typeOf(row.id, 'Number');
+        assert.equal(row.name, 'new-unique-key');
+        assert.equal(row.value, 33);
+
+        // Update the row
+        return table.insertUpdateUnique({
+          id: row.id,
+          value: 34
+        }).then(updatedRow => {
+          assert.equal(updatedRow.id, row.id);
+          assert.equal(updatedRow.name, 'new-unique-key');
+          assert.equal(updatedRow.value, 34);
+        });
+      });
+    });
+
+    it('should be possible to delete rows', () => {
+      return table.find({name: 'key1'}).remove().then(() => {
+        return table.find({name: 'key1'}).get();
+      }).then(row => {
+        assert.isUndefined(row);
+
+        // Restore the table
+        return table.insert({name: 'key1', value: 42});
+      });
+    });
+
+    it('should be allowed to create a table multiple times', () => {
+      return table.createTable().then(() => {
+        // Verify that old data still remains
+        return table.find({name: 'key1'}).get().then(row => {
+          assert.isObject(row);
+          assert.equal(row.name, 'key1');
+          assert.equal(row.value, 42);
+        });
+      });
+    });
+
+    it('should be possible to add columns to the table', () => {
+      let newTable = new sqlutil.Table(db, {
+        name: 'testtable', // same name as before
+        columns: {
+          id: {type: sqlutil.DataType.INTEGER, primaryKey: true},
+          name: {type: sqlutil.DataType.TEXT, unique: true},
+          value: {type: sqlutil.DataType.FLOAT},
+          value2: {type: sqlutil.DataType.FLOAT, index: true, defaultValue: 3.0}
+        }
+      });
+
+      return newTable.createTable().then(() => {
+        // Verify that old data still remains
+        return newTable.find({name: 'key1'}).get().then(row => {
+          assert.isObject(row);
+          assert.equal(row.name, 'key1');
+          assert.equal(row.value, 42);
+          assert.equal(row.value2, 3.0); // gets the default value for old rows
+        });
+      });
+    });
+
+    it('should be possible to remove columns from the table', () => {
+      let newTable = new sqlutil.Table(db, {
+        name: 'testtable', // same name as before
+        columns: {
+          id: {type: sqlutil.DataType.INTEGER, primaryKey: true},
+          name: {type: sqlutil.DataType.TEXT, unique: true},
+          value: {type: sqlutil.DataType.FLOAT}
+        }
+      });
+
+      return newTable.createTable().then(() => {
+        // Verify that old data still remains
+        return newTable.find({name: 'key1'}).get().then(row => {
+          assert.isObject(row);
+          assert.equal(row.name, 'key1');
+          assert.equal(row.value, 42);
+          assert.isUndefined(row.value2); // Column has been removed
+        });
+      });
+    });
+  });
+
+  describe('MapTable', () => {
+    let mapTable;
+
+    it('can be constructed', () => {
+      mapTable = new sqlutil.MapTable(db, 'testMapTable');
+      return mapTable.create();
+    });
+
+    it('should be possible to add values to the map', () => {
+      return Promise.all([
+        mapTable.set('key1', 'value1'),
+        mapTable.set('key2', 'value2'),
+        mapTable.set('key3', 'value3')
+      ]);
+    });
+
+    it('should be possible to retrieve values from the map', () => {
+      return mapTable.get('key1').then(value => {
+        assert.equal(value, 'value1');
+      });
+    });
+
+    it('should not return any values for undefined keys', () => {
+      return mapTable.get('undefined key').then(value => {
+        assert.typeOf(value, 'undefined');
+      });
+    });
+
+    it('should be possible to update keys in the map', () => {
+      return mapTable.set('key1', 'updated value1')
+        .then(() => mapTable.get('key1'))
+        .then(value => {
+          assert.equal(value, 'updated value1');
+        });
+    });
+  });
+});
